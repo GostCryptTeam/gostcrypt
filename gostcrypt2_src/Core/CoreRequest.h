b91bfca80d9b0248de5cc83f2d8e448605c28a9a
Quentin Varo <varo@et.esiea.fr>
2017-11-28 16:54:57 +0100

    make the constructors ProgressTrackingParameters explicit in CoreRequest.h

--- gostcrypt2_src/Core/CoreRequest.h
+++ gostcrypt2_src/Core/CoreRequest.h
@@ -31,8 +31,8 @@ static QString GetFileSystemTypePlatformNative () {
 struct ProgressTrackingParameters
 {
     explicit ProgressTrackingParameters(quint32 requestId) : requestId(requestId), start(0), end(1) {}
-    ProgressTrackingParameters() : start(0), end(1) {}
-    ProgressTrackingParameters(ProgressTrackingParameters &parent, qreal subStart, qreal subEnd) : requestId(parent.requestId), start(parent.end*subStart+parent.start*(1-subStart)), end(parent.end*subEnd+parent.start*(1-subEnd)) {}
+    explicit ProgressTrackingParameters() : start(0), end(1) {}
+    explicit ProgressTrackingParameters(ProgressTrackingParameters &parent, qreal subStart, qreal subEnd) : requestId(parent.requestId), start(parent.end*subStart+parent.start*(1-subStart)), end(parent.end*subEnd+parent.start*(1-subEnd)) {}
 
 
     quint32 requestId;
