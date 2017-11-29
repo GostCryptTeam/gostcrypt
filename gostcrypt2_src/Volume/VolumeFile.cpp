7485f76e9f257b4a035538a3e3001b0f2dd1bea6
Quentin Varo <varo@et.esiea.fr>
2017-11-29 18:00:55 +0100

    put a this before the variable preserveTimestamps

--- gostcrypt2_src/Volume/VolumeFile.cpp
+++ gostcrypt2_src/Volume/VolumeFile.cpp
@@ -104,7 +104,7 @@ void VolumeFile::Close ()
             throw;
 
 		Path = path;
-        preserveTimestamps = preserveTimestamps;
+        this->preserveTimestamps = preserveTimestamps;
 		FileIsOpen = true;
 	}
 
