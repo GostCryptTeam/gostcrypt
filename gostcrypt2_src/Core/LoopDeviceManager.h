abe0ac3f6b60fbb4e29f2639ae6df619282874a7
Antoine Hébert <ahebert@et.esiea.fr>
2018-03-04 18:20:26 +0100

    Document LoopDeviceManager.h and MountFilesystemManager.h

--- gostcrypt2_src/Core/LoopDeviceManager.h
+++ gostcrypt2_src/Core/LoopDeviceManager.h
@@ -6,10 +6,26 @@
 
 namespace GostCrypt {
     namespace Core {
+        /**
+         * @brief Class containing the methods managing the loop device. The loop device is a block file pointing to a regular file
+         *
+         */
         class LoopDeviceManager
         {
         public:
+            /**
+             * @brief Attach a loop device to the given imageFile
+             *
+             * @param imageFile Path of the image file to which the loop device should be attached
+             * @param readonly Boolean true if the loop device should be in read-only mode
+             * @return QFileInfo Path to the loop device now attached to the image file given in parameters
+             */
             static QFileInfo attachLoopDevice(QFileInfo imageFile, bool readonly);
+            /**
+             * @brief Detach a loop device from its regular file
+             *
+             * @param loopDevice Path to the loop device to detach
+             */
             static void detachLoopDevice(QFileInfo loopDevice);
         };
     }
