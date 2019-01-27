3cff06639c8a07850a4dab84d41b7770cb5f2165
Antoine Hébert <hantoine02@gmail.com>
2019-01-27 18:50:29 -0500

    Set up catch and AppVeyor so that test results are pushed to AppVeyor

--- src/CoreTestsAsRoot/tst_CoreRoot.cpp
+++ src/CoreTestsAsRoot/tst_CoreRoot.cpp
@@ -10,6 +10,9 @@ TEST_CASE("CoreRoot", "[Core]")
     QSharedPointer<CoreBase> core = getCore();
     SECTION(" can exit", "[exit]")
     {
+        QSignalSpy spy(core.data(), SIGNAL(exited()));
         core->exit();
+        CHECK(spy.wait());
+        CHECK(spy.count() == 1);
     }
 }
