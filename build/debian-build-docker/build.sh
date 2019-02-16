69cb178718077beaddb97e309c204ce71ea0fa7f
Antoine Hébert <hantoine02@gmail.com>
2019-02-16 09:57:15 -0500

    Add missing dependencies for Debian deb package

--- build/debian-build-docker/build.sh
+++ build/debian-build-docker/build.sh
@@ -3,4 +3,18 @@ mkdir build-debian
 cd build-debian
 qmake ../src/GostCrypt.pro
 make -j `cat /proc/cpuinfo | grep processor | wc -l` sub-UI
-fpm -s dir -t deb -n gostcrypt --version $APPVEYOR_BUILD_VERSION --license GPLv3 -m hantoine02@gmail.com -d qt5-default -d libqt5qml5 -d libqt5quick5 -d libblkid1 -d libfuse2 --deb-no-default-config-files GostCrypt=/usr/bin/gostcrypt
+fpm -s dir -t deb \
+	-n gostcrypt \
+       	--version $APPVEYOR_BUILD_VERSION \
+	--license GPLv3 \
+	-m hantoine02@gmail.com \
+	-d qt5-default \
+	-d libqt5qml5 \
+	-d qml-module-qtquick-controls \
+       	-d qml-module-qtquick-controls2 \
+       	-d qml-module-qtquick-templates \
+	-d qml-module-qtquick-dialogs \
+	-d libblkid1 \
+	-d libfuse2 \
+	--deb-no-default-config-files \
+	GostCrypt=/usr/bin/gostcrypt
