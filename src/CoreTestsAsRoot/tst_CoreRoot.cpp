bbbe21602c22a1de1eb91b2b91a2f6d7db187852
Antoine Hébert <hantoine02@gmail.com>
2019-02-24 11:19:20 -0500

    Add test that CoreRoot can mount a basic volume

--- src/CoreTestsAsRoot/tst_CoreRoot.cpp
+++ src/CoreTestsAsRoot/tst_CoreRoot.cpp
@@ -15,4 +15,24 @@ TEST_CASE("CoreRoot", "[Core]")
         CHECK(spy.wait());
         CHECK(spy.count() == 1);
     }
+    SECTION(" can mount volume", "[mountVolume]")
+    {
+        QSignalSpy spy(core.data(), SIGNAL(sendMountVolume(QSharedPointer<MountVolumeResponse>)));
+
+        QSharedPointer <GostCrypt::Core::MountVolumeRequest> mountRequest(new GostCrypt::Core::MountVolumeRequest);
+
+        mountRequest->doMount = true;
+        mountRequest->isDevice = false;
+        mountRequest->mountPoint = QFileInfo("/media/gostcrypt");
+        mountRequest->password.reset(new QByteArray("password"));
+        mountRequest->path = QFileInfo("./volume");
+
+
+        core->request(QVariant::fromValue(mountRequest));
+
+        CHECK(spy.wait());
+        CHECK(spy.count() == 1);
+        QSharedPointer<MountVolumeResponse> response = spy.takeFirst().first().value<QSharedPointer<MountVolumeResponse>>();
+        CHECK(response->readOnlyFailover == false);
+    }
 }
